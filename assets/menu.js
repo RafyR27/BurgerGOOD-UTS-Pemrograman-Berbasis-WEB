90dc460e426f29866f91a6f38bf88e017a6f99c5
RafyR27 <muhamadrafyr27@gmail.com>
2026-04-17 17:02:46 +0700

    feat: menu link to checkout

--- assets/menu.js
+++ assets/menu.js
@@ -22,6 +22,9 @@ const products = [
   },
 ];
 
+const btnAddCard = document.getElementById("btn-add-cart");
+const modal = document.getElementById("staticBackdrop");
+
 let count = 1;
 
 let addProduct = [];
@@ -29,6 +32,8 @@ let addProduct = [];
 let selectedData = null;
 
 document.addEventListener("DOMContentLoaded", function () {
+  addProduct = JSON.parse(localStorage.getItem("cart")) || [];
+
   const menuSection = document.getElementById("menu-section");
 
   menuSection.innerHTML = products
@@ -44,29 +49,31 @@ document.addEventListener("DOMContentLoaded", function () {
               style="width: 100px"
             />
             <div class="d-flex flex-column justify-content-center align-items-start gap-1">
-              <p class="fredoka-font-bold my-0">${item.name}</p>
+              <p class="fredoka-font-bold my-0 productName">${item.name}</p>
               <p class="fredoka-font my-0" style="font-size: 0.8rem">
                 ${item.descripsi}
               </p>
               <div class="w-100 d-flex flex-column flex-md-row flex-lg-row justify-content-between align-items-lg-center align-items-end mt-3">
-                <p class="my-0 fredoka-font">
+                <p class="my-0 fredoka-font w-100">
                   Rp. ${item.price.toLocaleString("id-ID")}
                 </p>
                 <div
-                    class="w-100 d-flex justify-content-end  align-items-center gap-3 mt-3 mt-md-2 mt-lg-0 d-none btn-card"
+                    class="w-100 d-flex justify-content-end align-items-center gap-3 mt-3 mt-md-2 mt-lg-0 d-none btn-card"
                   >
                     <button
                       type="button"
-                      class="btn rounded-5 btn-unactive btn-minus"
-                      style="padding: 6px 10px"
+                      class="btn rounded-5 btn-unactive-menu"
+                      style="padding: 6px 9px"
+                      onclick="delCountCard('${item.name}')"
                     >
                       <i class="bi bi-dash"></i>
                     </button>
                     <p class="my-0 quantity"></p>
                     <button
                       type="button"
-                      class="btn rounded-5 btn-unactive btn-plus"
-                      style="padding: 6px 10px"
+                      class="btn rounded-5 btn-unactive-menu"
+                      style="padding: 6px 9px"
+                      onclick="addCountCard('${item.name}')"
                     >
                       <i class="bi bi-plus"></i>
                     </button>
@@ -97,6 +104,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   detailButtons.forEach((button) => {
     button.addEventListener("click", function () {
+      const radios = document.querySelectorAll('input[name="radioDefault"]');
+      const name = document.getElementById("detailName");
+      const description = document.getElementById("detailDescription");
+      const image = document.getElementById("detailImage");
+      const price = document.getElementById("detailPrice");
+
       count = 1;
 
       modal.querySelector(".quantity").innerText = count;
@@ -105,11 +118,10 @@ document.addEventListener("DOMContentLoaded", function () {
         radio.checked = radio.value === "small";
       });
 
-      document.getElementById("detailName").innerText = this.dataset.name;
-      document.getElementById("detailDescription").innerText =
-        this.dataset.descripsi;
-      document.getElementById("detailImage").src = this.dataset.image;
-      document.getElementById("detailPrice").innerText =
+      name.innerText = this.dataset.name;
+      description.innerText = this.dataset.descripsi;
+      image.src = this.dataset.image;
+      price.innerText =
         "Rp. " + Number(this.dataset.price).toLocaleString("id-ID");
 
       selectedData = {
@@ -119,86 +131,43 @@ document.addEventListener("DOMContentLoaded", function () {
         unitPrice: Number(this.dataset.price),
         price: Number(this.dataset.price),
       };
+
+      radios.forEach((radio) => {
+        radio.addEventListener("change", function () {
+          const type = this.value;
+
+          const selectedProduct = products.find(
+            (p) => p.name === name.innerText,
+          );
+
+          if (selectedProduct) {
+            let finalPrice = selectedProduct.price;
+
+            if (type === "large") {
+              finalPrice += 10000;
+            }
+
+            if (selectedData) {
+              selectedData = {
+                name: selectedData.name,
+                type: this.value,
+                quantity: selectedData.quantity,
+                unitPrice: finalPrice,
+                price: finalPrice * selectedData.quantity,
+              };
+
+              price.innerText =
+                "Rp. " +
+                (finalPrice * selectedData.quantity).toLocaleString("id-ID");
+            }
+          }
+        });
+      });
     });
   });
 
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.forEach((item) => {
-    const card = document.querySelector(`[data-name="${item.name}"]`);
-
-    if (card) {
-      card.querySelector(".quantity").innerHTML = item.quantity;
-      card.querySelector(".btn-card").classList.remove("d-none");
-      card.querySelector(".btn-detail").classList.add("d-none");
-    }
-  });
-
-  if (cart.length > 0) {
-    document.getElementById("bar-checkout").classList.remove("d-none");
-
-    let totalPrice = 0;
-    let totalQuantity = 0;
-
-    for (let index = 0; index < cart.length; index++) {
-      totalPrice += cart[index].price;
-      totalQuantity += cart[index].quantity;
-    }
-
-    document.getElementById("bar-item").innerText = totalQuantity + " Item";
-    document.getElementById("bar-price").innerHTML =
-      "Rp. " +
-      Number(totalPrice).toLocaleString("id-ID") +
-      `<i class="bi bi-basket3-fill"></i>`;
-  } else {
-    document.getElementById("bar-checkout").classList.add("d-none");
-  }
+  updateCartDisplay();
 });
-
-const radios = document.querySelectorAll('input[name="radioDefault"]');
-
-radios.forEach((radio) => {
-  radio.addEventListener("change", function () {
-    const type = this.value;
-
-    const name = document.getElementById("detailName").innerText;
-    const price = document.getElementById("detailPrice");
-
-    const selectedProduct = products.find((p) => p.name === name);
-
-    if (selectedProduct) {
-      let finalPrice = selectedProduct.price;
-
-      if (type === "large") {
-        finalPrice += 10000;
-      }
-
-      if (selectedData) {
-        selectedData = {
-          name,
-          type,
-          quantity: count,
-          unitPrice: finalPrice,
-          price: finalPrice * selectedData.quantity,
-        };
-
-        price.innerText =
-          "Rp. " + (finalPrice * selectedData.quantity).toLocaleString("id-ID");
-      } else {
-        selectedData = {
-          name,
-          type,
-          quantity: count,
-          unitPrice: finalPrice,
-          price: finalPrice,
-        };
-
-        price.innerText = "Rp. " + finalPrice.toLocaleString("id-ID");
-      }
-    }
-  });
-});
-
-const modal = document.getElementById("staticBackdrop");
 
 const addCount = () => {
   count++;
@@ -215,66 +184,113 @@ const addCount = () => {
 };
 
 const delCount = () => {
-  count--;
+  if (count > 1) {
+    count--;
 
-  modal.querySelector(".quantity").innerText = count;
+    modal.querySelector(".quantity").innerText = count;
 
-  if (selectedData) {
-    selectedData.quantity = count;
-    selectedData.price = selectedData.unitPrice * count;
+    if (selectedData) {
+      selectedData.quantity = count;
+      selectedData.price = selectedData.unitPrice * count;
 
-    document.getElementById("detailPrice").innerText =
-      "Rp. " + selectedData.price.toLocaleString("id-ID");
+      document.getElementById("detailPrice").innerText =
+        "Rp. " + selectedData.price.toLocaleString("id-ID");
+    }
   }
 };
 
-modal.querySelector(".btn-plus").addEventListener("click", addCount);
-modal.querySelector(".btn-minus").addEventListener("click", () => {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+const addCountCard = (name) => {
+  if (addProduct.length > 0) {
+    addProduct = addProduct.map((item) => {
+      if (name === item.name) {
+        const newQuantity = item.quantity + 1;
 
-  console.log(this.dataset.name);
+        return {
+          ...item,
+          quantity: newQuantity,
+          price: item.unitPrice * newQuantity,
+        };
+      }
 
-  if (count == 1) {
-    return;
+      return item;
+    });
+
+    updateCartDisplay();
+  }
+};
+
+const delCountCard = (name) => {
+  if (addProduct.length > 0) {
+    addProduct = addProduct
+      .map((item) => {
+        if (name === item.name) {
+          const newQuantity = item.quantity - 1;
+
+          return {
+            ...item,
+            quantity: newQuantity,
+            price: item.unitPrice * newQuantity,
+          };
+        }
+
+        return item;
+      })
+      .filter((item) => item.quantity > 0);
+
+    updateCartDisplay();
   }
 
-  delCount();
-});
+  // console.log(addProduct);
+};
 
-const btnAddCard = document.getElementById("btn-add-cart");
+const updateCartDisplay = () => {
+  const cards = document.querySelectorAll(".menu-card");
 
-btnAddCard.addEventListener("click", () => {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cards.forEach((card) => {
+    const name = card.dataset.name;
+    const product = addProduct.find((item) => item.name === name);
 
-  cart.push(selectedData);
-
-  localStorage.setItem("cart", JSON.stringify(cart));
-
-  cart.forEach((item) => {
-    const card = document.querySelector(`[data-name="${item.name}"]`);
-
-    if (card) {
-      card.querySelector(".quantity").textContent = item.quantity;
+    if (product) {
+      card.querySelector(".quantity").innerHTML = product.quantity;
       card.querySelector(".btn-card").classList.remove("d-none");
       card.querySelector(".btn-detail").classList.add("d-none");
+    } else {
+      card.querySelector(".btn-card").classList.add("d-none");
+      card.querySelector(".btn-detail").classList.remove("d-none");
     }
   });
 
-  if (cart.length > 0) {
+  if (addProduct.length > 0) {
     document.getElementById("bar-checkout").classList.remove("d-none");
 
     let totalPrice = 0;
     let totalQuantity = 0;
 
-    for (let index = 0; index < cart.length; index++) {
-      totalPrice += cart[index].price;
-      totalQuantity += cart[index].quantity;
-    }
+    addProduct.forEach((item) => {
+      totalPrice += item.price;
+      totalQuantity += item.quantity;
+    });
 
     document.getElementById("bar-item").innerText = totalQuantity + " Item";
     document.getElementById("bar-price").innerHTML =
       "Rp. " +
-      Number(totalPrice).toLocaleString("id-ID") +
-      `<i class="bi bi-basket3-fill"></i>`;
+      totalPrice.toLocaleString("id-ID") +
+      ` <i class="bi bi-basket3-fill"></i>`;
+  } else {
+    document.getElementById("bar-checkout").classList.add("d-none");
+
+    localStorage.setItem("cart", JSON.stringify(addProduct));
   }
+};
+
+btnAddCard.addEventListener("click", () => {
+  addProduct.push(selectedData);
+
+  updateCartDisplay();
+});
+
+const btnCheckout = document.getElementById("btn-checkout");
+
+btnCheckout.addEventListener("click", () => {
+  localStorage.setItem("cart", JSON.stringify(addProduct));
 });
